f89171eb1434f189a6274e1ff80a8e9b4a612954
Aimée Craig <aimeecraig1@gmail.com>
2018-10-22 14:12:29 +0100

    initialized rspec capybara gemfile and added feature test

---
+++ spec/hello_spec.rb
@@ -0,0 +1,11 @@
+# As a user
+# So that I can quickly access websites
+# I'd like to view a list of bookmarks
+# ```
+
+feature 'Displays page' do
+  scenario 'Shows hello world' do
+    visit '/'
+    expect(page).to have_content 'Hello world!'
+  end
+end
