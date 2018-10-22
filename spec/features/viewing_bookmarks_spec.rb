a2c846586a7ebb1d77ac2a90df80ab6c7a6ec214
Aimée Craig <aimeecraig1@gmail.com>
2018-10-22 17:14:38 +0100

    test database implemented

--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -1,5 +1,7 @@
 feature 'Viewing bookmarks' do
   scenario 'it shows a list of bookmarks' do
+    connection = PG.connect(dbname: 'bookmark_manager_test')
+    connection.exec("INSERT INTO bookmarks VALUES(3, 'http://www.google.com');")
     visit '/bookmarks'
     expect(page).to have_content 'http://www.google.com'
   end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,12 +13,14 @@
 # it.
 #
 ENV['RACK_ENV'] = 'test'
+ENV['TEST_DATABASE'] = 'bookmark_manager_test'
 
 require 'capybara'
 require 'capybara/rspec'
 require 'rspec'
 require 'simplecov'
 require 'simplecov-console'
+require 'setup_test_database'
 
 require File.join(File.dirname(__FILE__), '..', 'app.rb')
 
@@ -33,6 +35,9 @@ SimpleCov.start
 
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
+  config.before(:each) do
+    setup_test_database
+  end
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
