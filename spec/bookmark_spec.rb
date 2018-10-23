5b78f9654992db7d61878e2b59f2d7666ac1e590
Aimée Craig <aimeecraig1@gmail.com>
2018-10-23 23:10:14 +0100

    submitting_bookmark_spec.rb renamed

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -1,12 +1,17 @@
 require 'bookmark'
 
 describe Bookmark do
-  subject(:bookmark) { Bookmark.new }
-
   describe '#all' do
     it 'returns a list of bookmarks' do
       bookmarks = Bookmark.all
       expect(bookmarks).to include "http://www.google.com"
+    end
+  end
+
+  describe '#create' do
+    it 'adds a new bookmark' do
+      subject.create('http://www.google.co.uk')
+      expect(subject.all).to include 'http://www.google.co.uk'
     end
   end
 end
