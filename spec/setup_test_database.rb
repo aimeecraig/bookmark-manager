09cb829dd00db85e5991f8c90f0db7c2cfbe6b49
Aimée Craig <aimeecraig1@gmail.com>
2018-10-24 09:46:07 +0100

    old code removed

--- spec/setup_test_database.rb
+++ spec/setup_test_database.rb
@@ -1,5 +1,4 @@
 def setup_test_database
   connection = PG.connect(dbname: ENV['TEST_DATABASE'])
   connection.exec("TRUNCATE bookmarks;")
-  # connection.exec("CREATE TABLE bookmarks( id SERIAL PRIMARY KEY, url VARCHAR(60));")
 end
